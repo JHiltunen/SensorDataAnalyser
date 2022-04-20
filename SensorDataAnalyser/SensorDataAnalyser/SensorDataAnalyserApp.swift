769e19743bcbe61be467a6b0d759122520892b40
Joonas Hiltunen <joonas.s.jh@gmail.com>
2022-04-20 10:53:50 +0300

    Add basic project and gitignore

---
+++ SensorDataAnalyser/SensorDataAnalyser/SensorDataAnalyserApp.swift
@@ -0,0 +1,17 @@
+//
+//  SensorDataAnalyserApp.swift
+//  SensorDataAnalyser
+//
+//  Created by iosdev on 20.4.2022.
+//
+
+import SwiftUI
+
+@main
+struct SensorDataAnalyserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
