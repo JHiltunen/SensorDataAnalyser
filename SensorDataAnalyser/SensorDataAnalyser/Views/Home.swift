c517567bb8459820e5ae748bff011ab3ddf4bde8
Miikka Huotari <miikka.huotari@metropolia.fi>
2022-04-28 09:53:43 +0300

    Ui changes

--- SensorDataAnalyser/SensorDataAnalyser/Views/Home.swift
+++ SensorDataAnalyser/SensorDataAnalyser/Views/Home.swift
@@ -7,11 +7,11 @@ struct Home: View {
     private let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
     
     
-    // Flexible, custom amount of columns that fill the remaining space
-    private let numberColumns = [
-        GridItem(.flexible()),
-        GridItem(.flexible())
-    ]
+//    // Flexible, custom amount of columns that fill the remaining space
+//    private let numberColumns = [
+//        GridItem(.flexible()),
+//        GridItem(.flexible())
+//    ]
     
     // Adaptive, make sure it's the size of your smallest element.
     private let adaptiveColumns = [
@@ -19,31 +19,35 @@ struct Home: View {
     ]
     
     var body: some View {
-        NavigationView {
+        ZStack {
+            RadialGradient(gradient: Gradient(colors: [.blue, .black]), center: .center, startRadius: 2, endRadius: 650)
+                .edgesIgnoringSafeArea(.all)
             ScrollView {
-                Text("Deviation")
-                    .frame(maxWidth: .infinity, minHeight: 50)
-                    .background(.indigo)
-                    .foregroundColor(.white)
-                    .cornerRadius(15)
-                    .padding(.bottom)
-                    .font(.system(size: 20, design: .rounded))
-                
-                LazyVGrid(columns: adaptiveColumns, spacing: 20) {
-                    ForEach(months, id: \.self) { month in
-                        ZStack {
-                            Rectangle()
-                                .frame(width: 150, height: 150)
-                                .foregroundColor(.indigo)
-                                .cornerRadius(15)
-                            VStack {
-                                Text("\(month)")
-                                    .foregroundColor(.white)
-                                    .font(.system(size: 25, design: .rounded))
-                                    .padding(.bottom)
-                                Text("Avg: \(String(Avg))")
-                                    .foregroundColor(.white)
-                                    .font(.system(size: 20, design: .rounded))
+                VStack {
+                    Text("Deviation")
+                        .frame(maxWidth: .infinity, minHeight: 50)
+                        .background(.indigo)
+                        .foregroundColor(.white)
+                        .cornerRadius(15)
+                        .padding(.bottom)
+                        .font(.system(size: 20, design: .rounded))
+                    
+                    LazyVGrid(columns: adaptiveColumns, spacing: 20) {
+                        ForEach(months, id: \.self) { month in
+                            ZStack {
+                                Rectangle()
+                                    .frame(width: 150, height: 150)
+                                    .foregroundColor(.indigo)
+                                    .cornerRadius(15)
+                                VStack {
+                                    Text("\(month)")
+                                        .foregroundColor(.white)
+                                        .font(.system(size: 25, design: .rounded))
+                                        .padding(.bottom)
+                                    Text("Avg: \(String(Avg))")
+                                        .foregroundColor(.white)
+                                        .font(.system(size: 20, design: .rounded))
+                                }
                             }
                         }
                     }
